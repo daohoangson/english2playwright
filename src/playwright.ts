d1c581882dc847cd643362998300b59f71bba796
Dao Hoang Son <daohoangson@gmail.com>
2023-11-10 22:06:06 +0700

    Convert png to jpeg if file size >500KB

--- src/playwright.ts
+++ src/playwright.ts
@@ -1,19 +1,33 @@
 import * as fs from "fs";
+import Jimp from "jimp";
 import { cleanUpScript } from "./prompts";
 
 const scriptToHighlight =
   "await page.evaluate(\"document.querySelectorAll('*').forEach((e) => { if (!e.id && !e.className) return; const rect = e.getBoundingClientRect(); const left = rect.left + window.scrollX; const top = rect.top + window.scrollY; const div = document.createElement('div'); div.innerText = ''; if (e.id) div.innerText += ` id=${e.id}`; if (e.className) div.innerText += ` class=${e.className}`; div.style = `background: black; color: white; font-size: 12px; position: absolute; top: ${top-12}px; left: ${left-6}px`; document.getElementsByTagName('body')[0].appendChild(div); });\");";
 
 export async function screenshot(script: string): Promise<string> {
-  const relativePath = `screenshots/screenshot-${Date.now()}.png`;
-  const scriptToScreenshot = `await page.screenshot({ path: './${relativePath}' });`;
+  const now = Date.now();
+  const jpegPath = `screenshots/${now}.jpeg`;
+  const pngPath = `screenshots/${now}.png`;
+  const scriptToScreenshot = `await page.screenshot({ path: './${pngPath}' });`;
 
   const evalScript = `${script};\n${scriptToHighlight}\n${scriptToScreenshot}\n${cleanUpScript}`;
   fs.writeFileSync("output.js", `${script}\n${cleanUpScript}`);
-  console.log(`playwright: Taking screenshot...`);
+  console.log(`playwright: Taking ${pngPath}...`);
   await eval(evalScript);
 
-  const buffer = fs.readFileSync(relativePath);
+  let bufferPath = pngPath;
+  const pngStat = fs.statSync(pngPath);
+  if (pngStat.size > 500000) {
+    // PNG file size is >500KB, most likely because of some raster image
+    console.log(`playwright: Writing ${jpegPath}...`);
+    const jimp = await Jimp.read(pngPath);
+    await jimp.quality(60).writeAsync(jpegPath);
+    bufferPath = jpegPath;
+  }
+
+  const buffer = fs.readFileSync(bufferPath);
   const base64 = buffer.toString("base64");
-  return "data:image/png;base64," + base64;
+  const mimeType = `image/${bufferPath === pngPath ? "png" : "jpeg"}`;
+  return `data:image/${mimeType};base64,${base64}`;
 }
