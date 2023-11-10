0f6793a9e163440b33913ef69d2b8de0058896e4
Dao Hoang Son <daohoangson@gmail.com>
2023-11-10 21:51:47 +0700

    Switch to use data URI for screenshot image

--- src/playwright.ts
+++ src/playwright.ts
@@ -5,12 +5,6 @@ const scriptToHighlight =
   "await page.evaluate(\"document.querySelectorAll('*').forEach((e) => { if (!e.id && !e.className) return; const rect = e.getBoundingClientRect(); const left = rect.left + window.scrollX; const top = rect.top + window.scrollY; const div = document.createElement('div'); div.innerText = ''; if (e.id) div.innerText += ` id=${e.id}`; if (e.className) div.innerText += ` class=${e.className}`; div.style = `background: black; color: white; font-size: 12px; position: absolute; top: ${top-12}px; left: ${left-6}px`; document.getElementsByTagName('body')[0].appendChild(div); });\");";
 
 export async function screenshot(script: string): Promise<string> {
-  const ngrokUrl = process.env.NGROK_URL ?? "";
-  if (ngrokUrl.length === 0) {
-    console.error("NGROK_URL is not set");
-    process.exit(1);
-  }
-
   const relativePath = `screenshots/screenshot-${Date.now()}.png`;
   const scriptToScreenshot = `await page.screenshot({ path: './${relativePath}' });`;
 
@@ -19,5 +13,7 @@ export async function screenshot(script: string): Promise<string> {
   console.log(`playwright: Taking screenshot...`);
   await eval(evalScript);
 
-  return `${ngrokUrl}/${relativePath}`;
+  const buffer = fs.readFileSync(relativePath);
+  const base64 = buffer.toString("base64");
+  return "data:image/png;base64," + base64;
 }
